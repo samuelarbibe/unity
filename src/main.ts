47520cde4c5434a12784017d55bafb6af83a7718
Samuel Arbibe <samuel.arbibe@gmail.com>
2023-10-04 20:52:28 +0300

    Fix usage of Vector3.applyAxisAngle

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import {
   get3DObjectFromPolygon,
   getFrustumIntersection,
   get3DObjectFromLineString,
+  getFrustumNormal,
 } from './utils';
 import { GlobeGeometry } from './Globe.ts';
 import lebanonDistricts from '../data/lebanon.ts'
@@ -31,7 +32,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 const lebanonGeoJSON = mergePolygons(lebanonDistricts, 0.01)
 const lebanon = get3DObjectFromPolygon(lebanonGeoJSON.geometry);
 
-const globeGeometry = new GlobeGeometry(0.1, 0.1, 0, 37, 32.5, 60);
+const globeGeometry = new GlobeGeometry(0.1, 0.1, 34, 37, 32.5, 35);
 const globeMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true, transparent: true, opacity: 0.05, side: THREE.BackSide });
 const globe = new THREE.Mesh(globeGeometry, globeMaterial);
 
@@ -39,40 +40,59 @@ const laneGeoJSON = lineString([
   [
     35.070771710910634,
     33.2583547951523,
-    4500
+    3500
   ],
   [
     35.29505172516929,
     33.80528180324596,
-    4500
+    3500
+  ],
+  [
+    35.23505172516929,
+    33.86528180324596,
+    3500
   ]
-]
-)
-const slerpDistance = METERS_PER_KM * 100
+])
+const slerpDistance = METERS_PER_KM * 1
 const lane = get3DObjectFromLineString(laneGeoJSON, slerpDistance)
 
-const target = lngLatAltToVector([35.39428495959217, 33.224181124642186])
+const cameraFocus = lngLatAltToVector([35.39428495959217, 33.224181124642186])
 
-for (let i = 0; i <= 0; i++) {
-  const origin = getPointOnLine(lane, i)
-  const intersectionPoints = getFrustumIntersection(origin, target, 1, 10, globe)
+let index = 0
+const origin = getPointOnLine(lane, index++)
+const nextPosition = getPointOnLine(lane, index)
 
-  const geometry = new THREE.BufferGeometry().setFromPoints([...intersectionPoints, intersectionPoints[0]]);
-  const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+while (origin.x && nextPosition.x) {
+  const direction = nextPosition.clone().sub(origin).normalize()
 
-  const intersection = new THREE.Line(geometry, material);
+  for (let v = 8; v <= 8; v += 5) {
+    for (let h = 80; h <= 100; h += 5) {
+      const frustumNormal = getFrustumNormal(origin, direction, v, h)
+      const target = frustumNormal.clone().add(origin)
+      const intersectionPoints = getFrustumIntersection(origin, target, 3, 3, globe)
 
-  scene.add(intersection)
+      const closedIntersectionPoints = [...intersectionPoints, intersectionPoints[0]].filter(Boolean)
+      const geometry = new THREE.BufferGeometry().setFromPoints(closedIntersectionPoints);
+      const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+
+      const intersection = new THREE.Line(geometry, material);
+
+      scene.add(intersection)
+    }
+  }
+
+  origin.copy(nextPosition)
+  getPointOnLine(lane, index++, nextPosition)
 }
 
 scene.add(lane);
 scene.add(globe);
 scene.add(lebanon);
 
-const cameraPosition = target.clone().addScalar(2)
+const cameraPosition = cameraFocus.clone().addScalar(2)
 
 camera.position.copy(cameraPosition)
-controls.target.copy(target)
+controls.target.copy(cameraFocus)
 
 // const axesHelper = new THREE.AxesHelper(lngLatAltToVector([0, 0, 0]).x);
 
--- src/utils.ts
+++ src/utils.ts
@@ -154,16 +154,39 @@ export function getPointOnLine(line: Line, index: number, result: Vector3 = new 
   return result
 }
 
+export function getFrustumNormal(
+  from: Vector3,
+  movingDirection: Vector3,
+  verticalAngle: number = 90,
+  horizontalAngle: number = 0
+) {
+  const up = new Vector3(...Ellipsoid.WGS84.geodeticSurfaceNormal(from.toArray())).normalize()
+
+  const xAxis = movingDirection
+  const zAxis = xAxis.clone().cross(up)
+  const yAxis = zAxis.clone().cross(xAxis)
+
+  const frustumNormal = xAxis.clone()
+
+  frustumNormal
+    .applyAxisAngle(zAxis, toRadians(-verticalAngle))
+    .applyAxisAngle(yAxis, toRadians(-horizontalAngle))
+    .normalize()
+
+  return frustumNormal
+}
+
 export function createFrustumFrame(from: Vector3, to: Vector3, alpha: number, beta: number) {
-  const yAxis = new Vector3(...Ellipsoid.WGS84.geodeticSurfaceNormal(from.toArray())).normalize()
-  const center = to.clone().sub(from).normalize()
+  const up = new Vector3(...Ellipsoid.WGS84.geodeticSurfaceNormal(from.toArray())).normalize()
 
-  const zAxis = center.clone().cross(yAxis).normalize()
+  const xAxis = to.clone().sub(from).normalize()
+  const zAxis = xAxis.clone().cross(up).normalize().negate()
+  const yAxis = xAxis.clone().cross(zAxis).normalize()
 
-  const topRight = center.clone().applyAxisAngle(zAxis, toRadians(alpha)).applyAxisAngle(yAxis, toRadians(beta)).normalize()
-  const bottomRight = center.clone().applyAxisAngle(zAxis, toRadians(-alpha)).applyAxisAngle(yAxis, toRadians(beta)).normalize()
-  const topLeft = center.clone().applyAxisAngle(zAxis, toRadians(alpha)).applyAxisAngle(yAxis, toRadians(-beta)).normalize()
-  const bottomLeft = center.clone().applyAxisAngle(zAxis, toRadians(-alpha)).applyAxisAngle(yAxis, toRadians(-beta)).normalize()
+  const topRight = xAxis.clone().applyAxisAngle(yAxis, toRadians(alpha)).applyAxisAngle(zAxis, toRadians(beta)).normalize()
+  const bottomRight = xAxis.clone().applyAxisAngle(yAxis, toRadians(-alpha)).applyAxisAngle(zAxis, toRadians(beta)).normalize()
+  const topLeft = xAxis.clone().applyAxisAngle(yAxis, toRadians(alpha)).applyAxisAngle(zAxis, toRadians(-beta)).normalize()
+  const bottomLeft = xAxis.clone().applyAxisAngle(yAxis, toRadians(-alpha)).applyAxisAngle(zAxis, toRadians(-beta)).normalize()
 
   return {
     topRight,
@@ -185,13 +208,13 @@ export function getFrustumIntersection(
   const frustumFrame = createFrustumFrame(from, to, alpha, beta)
 
   raycaster.set(from, frustumFrame.topRight.clone().normalize())
-  intersectionPoints.push(raycaster.intersectObject(object)[0].point)
+  intersectionPoints.push(raycaster.intersectObject(object)[0]?.point)
   raycaster.set(from, frustumFrame.bottomRight.clone().normalize())
-  intersectionPoints.push(raycaster.intersectObject(object)[0].point)
+  intersectionPoints.push(raycaster.intersectObject(object)[0]?.point)
   raycaster.set(from, frustumFrame.bottomLeft.clone().normalize())
-  intersectionPoints.push(raycaster.intersectObject(object)[0].point)
+  intersectionPoints.push(raycaster.intersectObject(object)[0]?.point)
   raycaster.set(from, frustumFrame.topLeft.clone().normalize())
-  intersectionPoints.push(raycaster.intersectObject(object)[0].point)
+  intersectionPoints.push(raycaster.intersectObject(object)[0]?.point)
 
-  return intersectionPoints
+  return intersectionPoints.filter(Boolean)
 }
